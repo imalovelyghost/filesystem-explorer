2c92848bbcad803b0e8e7dc52cc229a3518a2120
Brahim <brahimcasas@hotmail.es>
2021-06-23 13:55:44 +0200

    Feat: handle add button

--- index.php
+++ index.php
@@ -42,7 +42,11 @@
                                     </div>
                                     <div class="modal-body">
                                         <!-- <label data-error="wrong" data-success="right" for="defaultForm-email">Your email</label> -->
-                                        <form action="./modules/create_directory.php" method="post">
+                                        <?php
+                                        $file = $_GET['file'] ?? '';
+                                        $path = "./modules/create_directory.php?file=" . $file;
+                                        ?>
+                                        <form action=<?= $path ?> method="post">
                                             <!-- <input type="submit" class="btn btn-primary" value="+ Add File"> -->
                                             <input type="text" id="defaultForm-name" name="directory-name" placeholder="Insert directory name" class="form-control validate">
 
@@ -60,7 +64,7 @@
                         <h5 class="my-3">File Explorer</h5>
                         <div class="fm-menu">
                             <div class="list-group list-group-flush"> <a href="javascript:;" class="list-group-item py-1"><i class="bx bx-folder me-2"></i><span>All Files</span></a>
-                                <a href="javascript:;" class="list-group-item py-1"><i class="bx bx-devices me-2"></i><span>Root</span></a>
+                                <a href="./" class="list-group-item py-1"><i class="bx bx-devices me-2"></i><span>Root</span></a>
                                 <a href="javascript:;" class="list-group-item py-1"><i class="bx bx-analyse me-2"></i><span>Documents</span></a>
                                 <a href="javascript:;" class="list-group-item py-1"><i class="bx bx-plug me-2"></i><span>Images</span></a>
                                 <a href="javascript:;" class="list-group-item py-1"><i class="bx bx-plug me-2"></i><span>Audio</span></a>
@@ -102,15 +106,13 @@
                                         <!-- <div class="font-30 text-primary"><i class="bx bxs-folder"></i></div> -->
                                         <?php
                                         $file = $_GET["file"] ?? "";
-                                        // echo $file;
                                         $arrFiles = getFilesInfo($file);
                                         if (is_array($arrFiles)) {
                                             foreach ($arrFiles as $entry) {
                                         ?>
                                                 <div class="row">
                                                     <div class="col-sm">
-                                                        <?= fileIcon($entry); ?>
-                                                        <a href=<?= "?file=" . $entry["name"] ?>><?= $entry["name"] ?></a>
+                                                        <?= getAnchor($entry); ?>
                                                     </div>
                                                     <div class="col-sm">
                                                         <?= gmdate("Y-m-d\ H:i:s", $entry["ctime"]); ?>
@@ -151,7 +153,7 @@
 <?php
 function getFilesInfo($path)
 {
-    $directory  = (__DIR__ . "\\root\\" . $path);
+    $directory  = (__DIR__ . "/root/" . $path);
     $scanned_directory = array_diff(scandir($directory), array('..', '.'));
 
     foreach ($scanned_directory as $entry) {
@@ -171,6 +173,15 @@ function getFilesInfo($path)
     }
 
     return $result ?? 'This folder is empty';
+}
+
+function getAnchor($entry)
+{
+    $name = $entry["name"];
+    $href = isset($_GET['file']) ? $_GET['file'] . '/' . $name :  $name;
+
+    echo fileIcon($entry);
+    echo "<a href= \"?file=$href\"> $name</a>";
 }
 
 function fileIcon($file)
