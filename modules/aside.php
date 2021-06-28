0875c2af46127683d6fe3f67e401b02b239a25ac
imalovelyghost <imalovelyghost13@gmail.com>
2021-06-28 12:04:11 +0200

    async mode started

--- modules/aside.php
+++ modules/aside.php
@@ -36,8 +36,20 @@ $path = "./scripts/manage_dir.php?file=" . $file;
                     </form>
                 </div>
             </div>
-        </div>
+        </div><br><br>
         <!-- Modal -->
+
+        <!-- Upload Async Test -->
+
+        <form enctype="multipart/form-data">
+            <input name="file" type="file" /><br><br>
+            <input type="button" value="Upload" /><br><br>
+        </form>
+        <progress></progress><br><br>
+
+        <!-- END -->
+
+
 
         <h5 class="my-3">File Explorer</h5>
         <div class="fm-menu">
@@ -73,4 +85,52 @@ $path = "./scripts/manage_dir.php?file=" . $file;
     $('#exampleModalCenter').on('shown.bs.modal', function() {
         $('#defaultForm-name').trigger('focus')
     })
+
+
+    //Upload Async
+
+    $(':file').on('change', function() {
+        var file = this.files[0];
+
+        if (file.size > 1024) {
+            alert('max upload size is 1k');
+        }
+        console.log(file.size);
+
+        // Also see .name, .type
+    });
+
+    $(':button').on('click', function() {
+        $.ajax({
+            // Your server script to process the upload
+            url: 'aside.php',
+            type: 'POST',
+
+            // Form data
+            data: new FormData($('form')[0]),
+
+            // Tell jQuery not to process data or worry about content-type
+            // You *must* include these options!
+            cache: false,
+            contentType: false,
+            processData: false,
+
+            // Custom XMLHttpRequest
+            xhr: function() {
+                var myXhr = $.ajaxSettings.xhr();
+                if (myXhr.upload) {
+                    // For handling the progress of the upload
+                    myXhr.upload.addEventListener('progress', function(e) {
+                        if (e.lengthComputable) {
+                            $('progress').attr({
+                                value: e.loaded,
+                                max: e.total,
+                            });
+                        }
+                    }, false);
+                }
+                return myXhr;
+            }
+        });
+    });
 </script>
